986d9caf66ac1fce6a41954ba348079e47790274
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-07-15 00:27:05 -0500

    Parte 2, 3, 4: prop, useState y Listar peliculas con map

--- Mis peliculas favoritas/src/App.jsx
+++ Mis peliculas favoritas/src/App.jsx
@@ -1,17 +1,47 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
+function Pelicula({titulo}) {
+  const [favorita, setFavorita] = useState(false);
 
+  return (
+    <div style={{
+        backgroundColor: favorita ? "#ffe082" : "#f4f4f4",
+        padding: "15px",
+        margin: "10px 0",
+        borderRadius: "8px",
+        cursor: "pointer",
+        fontSize: "20px",
+      }}
 
+      onClick={() => setFavorita(!favorita)}
+    >
+      {titulo} {favorita ? "⭐" : ""}
+    </div>
+  );
+}
 
 function App() {
+  
+  const peliculas = [
+    "Bohemian Rhapsody",
+    "La teoria del todo",
+    "Avengers: Endgame",
+    "Juego de honor"]
+  
+
 
   return (
     <div>
-      <h1>Mis peliculas favoritas</h1>
+      <h1>Mis Películas Favoritas</h1>
+
+      <div style={{ marginTop: "20px" }}>
+        {peliculas.map((pelicula, indice) => (
+          <Pelicula key={indice} titulo={pelicula} />
+        ))}
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
