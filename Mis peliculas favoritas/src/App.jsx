db8371fe1cc501a31bd5e0d209e97f0e022c74fc
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-07-15 00:29:41 -0500

    Parte5: Agregar peliculas con boton

--- Mis peliculas favoritas/src/App.jsx
+++ Mis peliculas favoritas/src/App.jsx
@@ -22,18 +22,50 @@ function Pelicula({titulo}) {
 }
 
 function App() {
-  
-  const peliculas = [
+  const [peliculas, setPeliculas] = useState([
     "Bohemian Rhapsody",
     "La teoria del todo",
     "Avengers: Endgame",
-    "Juego de honor"]
-  
+    "Juego de honor",
+  ]);
 
+  const [nuevaPelicula, setNuevaPelicula] = useState("");
+
+  function agregarPelicula() {
+    if (nuevaPelicula.trim() === "") {
+      return;
+    }else{
+
+    setPeliculas([...peliculas, nuevaPelicula]);
+    setNuevaPelicula("");
+    }
+  }
 
   return (
     <div>
       <h1>Mis Películas Favoritas</h1>
+
+      <input
+        type="text"
+        placeholder="Nueva película"
+        value={nuevaPelicula}
+        onChange={(e) => setNuevaPelicula(e.target.value)}
+        style={{
+          padding: "10px",
+          width: "70%",
+          fontSize: "16px",
+        }}
+      />
+
+      <button onClick={agregarPelicula}
+        style={{
+          padding: "10px 15px",
+          marginLeft: "10px",
+          cursor: "pointer",
+        }}
+      >
+        Agregar
+      </button>
 
       <div style={{ marginTop: "20px" }}>
         {peliculas.map((pelicula, indice) => (
